8d5b3305066a40c4b0ece7177c9291a138fc5c4e
frstrtr <frstrtr@gmail.com>
2020-07-15 09:49:26 +0300

    BBANDS nicehash adoption

--- Chapter2/bbands.py
+++ Chapter2/bbands.py
@@ -1,20 +1,23 @@
+import matplotlib.pyplot as plt
+import math as math
+import statistics as stats
 import pandas as pd
 
 from pandas_datareader import data
 
 start_date = '2014-01-01'
 end_date = '2018-01-01'
-SRC_DATA_FILENAME = 'goog_data.pkl'
+SRC_DATA_FILENAME = 'nicehash_ETHBTC_data_days.pk1'
 
 try:
-  goog_data2 = pd.read_pickle(SRC_DATA_FILENAME)
+    goog_data2 = pd.read_pickle(SRC_DATA_FILENAME)
 except FileNotFoundError:
-  goog_data2 = data.DataReader('GOOG', 'yahoo', start_date, end_date)
-  goog_data2.to_pickle(SRC_DATA_FILENAME)
+    goog_data2 = data.DataReader('GOOG', 'yahoo', start_date, end_date)
+    goog_data2.to_pickle(SRC_DATA_FILENAME)
 
-goog_data = goog_data2.tail(620)
+goog_data = goog_data2  # .tail(620)
 
-close = goog_data['Close']
+close = goog_data['close']
 
 '''
 The Bollinger Band (BBANDS) study created
@@ -59,43 +62,46 @@ Middle Band = MA
 Lower Band = MA - delta
 
  '''
-import statistics as stats
-import math as math
 
-time_period = 20 # history length for Simple Moving Average for middle band
-stdev_factor = 2 # Standard Deviation Scaling factor for the upper and lower bands
-history = [] # price history for computing simple moving average
-sma_values = [] # moving average of prices for visualization purposes
-upper_band = [] # upper band values
-lower_band = [] # lower band values
+time_period = 20  # history length for Simple Moving Average for middle band
+stdev_factor = 2  # Standard Deviation Scaling factor for the upper and lower bands
+history = []  # price history for computing simple moving average
+sma_values = []  # moving average of prices for visualization purposes
+upper_band = []  # upper band values
+lower_band = []  # lower band values
 
 for close_price in close:
-  history.append(close_price)
-  if len(history) > time_period: # we only want to maintain at most 'time_period' number of price observations
-    del (history[0])
+    history.append(close_price)
+    # we only want to maintain at most 'time_period' number of price observations
+    if len(history) > time_period:
+        del (history[0])
 
-  sma = stats.mean(history)
-  sma_values.append(sma) # simple moving average or middle band
-  variance = 0 # variance is the square of standard deviation
-  for hist_price in history:
-    variance = variance + ((hist_price - sma) ** 2)
+    sma = stats.mean(history)
+    sma_values.append(sma)  # simple moving average or middle band
+    variance = 0  # variance is the square of standard deviation
+    for hist_price in history:
+        variance = variance + ((hist_price - sma) ** 2)
 
-  stdev = math.sqrt(variance / len(history)) # use square root to get standard deviation
+    # use square root to get standard deviation
+    stdev = math.sqrt(variance / len(history))
 
-  upper_band.append(sma + stdev_factor * stdev)
-  lower_band.append(sma - stdev_factor * stdev)
+    upper_band.append(sma + stdev_factor * stdev)
+    lower_band.append(sma - stdev_factor * stdev)
 
-goog_data = goog_data.assign(ClosePrice=pd.Series(close, index=goog_data.index))
-goog_data = goog_data.assign(MiddleBollingerBand20DaySMA=pd.Series(sma_values, index=goog_data.index))
-goog_data = goog_data.assign(UpperBollingerBand20DaySMA2StdevFactor=pd.Series(upper_band, index=goog_data.index))
-goog_data = goog_data.assign(LowerBollingerBand20DaySMA2StdevFactor=pd.Series(lower_band, index=goog_data.index))
+goog_data = goog_data.assign(
+    ClosePrice=pd.Series(close, index=goog_data.index))
+goog_data = goog_data.assign(
+    MiddleBollingerBand20DaySMA=pd.Series(sma_values, index=goog_data.index))
+goog_data = goog_data.assign(UpperBollingerBand20DaySMA2StdevFactor=pd.Series(
+    upper_band, index=goog_data.index))
+goog_data = goog_data.assign(LowerBollingerBand20DaySMA2StdevFactor=pd.Series(
+    lower_band, index=goog_data.index))
 
 close_price = goog_data['ClosePrice']
 mband = goog_data['MiddleBollingerBand20DaySMA']
 uband = goog_data['UpperBollingerBand20DaySMA2StdevFactor']
 lband = goog_data['LowerBollingerBand20DaySMA2StdevFactor']
 
-import matplotlib.pyplot as plt
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111, ylabel='Google price in $')
