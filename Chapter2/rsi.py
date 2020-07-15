9485e9917a9292fbc8fa6d1fb53b7faaf5648116
frstrtr <frstrtr@gmail.com>
2020-07-15 09:57:16 +0300

    RSI nicehash adoption

--- Chapter2/rsi.py
+++ Chapter2/rsi.py
@@ -4,7 +4,7 @@ from pandas_datareader import data
 
 start_date = '2014-01-01'
 end_date = '2018-01-01'
-SRC_DATA_FILENAME = 'goog_data.pkl'
+SRC_DATA_FILENAME = 'nicehash_ETHBTC_data_days.pk1'
 
 try:
   goog_data2 = pd.read_pickle(SRC_DATA_FILENAME)
@@ -12,9 +12,9 @@ except FileNotFoundError:
   goog_data2 = data.DataReader('GOOG', 'yahoo', start_date, end_date)
   goog_data2.to_pickle(SRC_DATA_FILENAME)
 
-goog_data = goog_data2.tail(620)
+goog_data = goog_data2#.tail(620)
 
-close = goog_data['Close']
+close = goog_data['close']
 
 '''
 The Relative Strength Index (RSI) was published
@@ -78,7 +78,7 @@ rsi = goog_data['RelativeStrengthIndicatorOver20Days']
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
-ax1 = fig.add_subplot(311, ylabel='Google price in $')
+ax1 = fig.add_subplot(311, ylabel='ETH price in BTC')
 close_price.plot(ax=ax1, color='black', lw=2., legend=True)
 ax2 = fig.add_subplot(312, ylabel='RS')
 rs_gain.plot(ax=ax2, color='g', lw=2., legend=True)
