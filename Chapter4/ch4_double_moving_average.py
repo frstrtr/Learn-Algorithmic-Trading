fa95d838b2d9f1d464116743b3f235367474a578
frstrtr <frstrtr@gmail.com>
2020-07-23 18:56:23 +0300

    dual moving average nicehash adoption

--- Chapter4/ch4_double_moving_average.py
+++ Chapter4/ch4_double_moving_average.py
@@ -10,23 +10,24 @@ def load_financial_data(start_date, end_date,output_file):
         print('File data found...reading GOOG data')
     except FileNotFoundError:
         print('File not found...downloading the GOOG data')
-        df = data.DataReader('GOOG', 'yahoo', start_date, end_date)
-        df.to_pickle(output_file)
+        # df = data.DataReader('GOOG', 'yahoo', start_date, end_date)
+        # df.to_pickle(output_file)
+        df = None
     return df
 
 goog_data=load_financial_data(start_date='2001-01-01',
                     end_date = '2018-01-01',
-                    output_file='goog_data.pkl')
+                    output_file='nicehash_ETHBTC_data_days.pk1')
 
 
 
 def double_moving_average(financial_data, short_window, long_window):
     signals = pd.DataFrame(index=financial_data.index)
     signals['signal'] = 0.0
-    signals['short_mavg'] = financial_data['Close'].\
+    signals['short_mavg'] = financial_data['close'].\
         rolling(window=short_window,
                 min_periods=1, center=False).mean()
-    signals['long_mavg'] = financial_data['Close'].\
+    signals['long_mavg'] = financial_data['close'].\
         rolling(window=long_window,
                 min_periods=1, center=False).mean()
     signals['signal'][short_window:] =\
@@ -38,17 +39,17 @@ def double_moving_average(financial_data, short_window, long_window):
 ts=double_moving_average(goog_data,20,100)
 
 fig = plt.figure()
-ax1 = fig.add_subplot(111, ylabel='Google price in $')
-goog_data["Adj Close"].plot(ax=ax1, color='g', lw=.5)
+ax1 = fig.add_subplot(111, ylabel='ETH price in BTC')
+goog_data["close"].plot(ax=ax1, color='g', lw=.5)
 ts["short_mavg"].plot(ax=ax1, color='r', lw=2.)
 ts["long_mavg"].plot(ax=ax1, color='b', lw=2.)
 
 ax1.plot(ts.loc[ts.orders== 1.0].index,
-         goog_data["Adj Close"][ts.orders == 1.0],
+         goog_data["close"][ts.orders == 1.0],
          '^', markersize=7, color='k')
 
 ax1.plot(ts.loc[ts.orders== -1.0].index,
-         goog_data["Adj Close"][ts.orders == -1.0],
+         goog_data["close"][ts.orders == -1.0],
          'v', markersize=7, color='k')
 
 plt.legend(["Price","Short mavg","Long mavg","Buy","Sell"])
@@ -62,7 +63,7 @@ sys.exit(0)
 
 # You are going to set your initial amount of money you want
 # to invest --- here it is 10,000
-initial_capital = float(10000.0)
+initial_capital = float(7.0)
 
 # You are going to create a new dataframe positions
 # Remember the index is still the same as signals
@@ -72,23 +73,23 @@ positions = pd.DataFrame(index=signals.index).fillna(0.0)
 # You are going to sell 10 shares of MSFT when signal is -1
 # You will assign these values to the column MSFT of the
 # dataframe positions
-positions['MSFT'] = 10 * signals['signal']
+positions['ETH'] = 10 * signals['signal']
 
 # You are now going to calculate the notional (quantity x price)
 # for your portfolio. You will multiply Adj Close from
 # the dataframe containing prices and the positions (10 shares)
 # You will store it into the variable portfolio
-portfolio = positions.multiply(financial_data['Adj Close'], axis=0)
+portfolio = positions.multiply(financial_data['close'], axis=0)
 
 # Add `holdings` to portfolio
-portfolio['holdings'] = (positions.multiply(financial_data['Adj Close'], axis=0)).sum(axis=1)
+portfolio['holdings'] = (positions.multiply(financial_data['close'], axis=0)).sum(axis=1)
 
 # You will store positions.diff into pos_diff
 pos_diff = positions.diff()
 # You will now add a column cash in your dataframe portfolio
 # which will calculate the amount of cash you have
 # initial_capital - (the notional you use for your different buy/sell)
-portfolio['cash'] = initial_capital - (pos_diff.multiply(financial_data['Adj Close'], axis=0)).sum(axis=1).cumsum()
+portfolio['cash'] = initial_capital - (pos_diff.multiply(financial_data['close'], axis=0)).sum(axis=1).cumsum()
 
 # You will now add a column total to your portfolio calculating the part of holding
 # and the part of cash
